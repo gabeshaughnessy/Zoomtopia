a0c46d55b51791dd77879e4a5635e226cd32174c
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-11-05 19:39:57 -0800

    twenty thirteen and child

---
+++ wp-content/themes/twentythirteen-child/templates/page-template.php
@@ -0,0 +1,29 @@
+<?php
+/** 
+* Template Name: Page Alternate
+*
+* @package WordPress
+* @subpackage Twenty_Thirteen
+* @since Twenty Thirteen 1.0
+*/
+
+get_header(); ?>
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+			
+			<?php //remove_filter( 'the_content', 'wpautop' ); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
+
+				</article><!-- #post -->
+			<?php endwhile; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
